499e01a957efc5b570375e4ded1b76eac9070d71
sajmonowski115 <szymekbobrowski@wp.pl>
2020-03-10 08:23:13 +0100

    tryAndExcept

---
+++ basics/tryAndExcept.py
@@ -0,0 +1,16 @@
+#This program demonstrates use of try function
+#The function recognizes when there is an error in the program
+#Than it stops running the code and continues to the except statement
+
+i = 1
+
+while(i):
+    try:
+        x = int(input("Type a number: "))
+        i=0
+    except:
+        print("Something is wrong. Try again")
+        i=1
+
+input("Program finished successfully. Press enter to continue...")
+        
